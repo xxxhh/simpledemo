6d5863164fb6ee18bca6fbbdb8897f600d1ebe9e
secdn <toxxxhh@gmail.com>
2017-08-01 22:32:29 +0800

    框架搭建

---
+++ src/main/java/org/secdn/simpledemo/SimpledemoApplication.java
@@ -0,0 +1,12 @@
+package org.secdn.simpledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpledemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpledemoApplication.class, args);
+	}
+}
